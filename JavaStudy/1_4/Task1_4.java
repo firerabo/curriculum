6adb16b7b0e75155cbdd806efaa8d9bb1bb8b366
firerabo <80002988+firerabo@users.noreply.github.com>
2021-04-09 04:54:22 +0900

    課題提出
    制御文（条件分岐）課題

--- JavaStudy/1_4/Task1_4.java
+++ JavaStudy/1_4/Task1_4.java
@@ -39,18 +39,18 @@ public class Task1_4 {
 
         // ② 「name」の値が「USER_RNAME」と等しく、「 pass 」の値が「USER_PASSWORD」と等しい場合。
 	    //定数を使用して「 ログイン成功です 」と出力してください。
-    if (USER_RNAME.equals(name) && USER_PASSWORD.equals("pass")) {
+    if (USER_RNAME.equals(name) && USER_PASSWORD.equals(pass)) {
     System.out.println(CONST_MSG_SUCCESS); 
 
 
         // ③ 「USER_RNAME」の値のみ等しい場合。
        //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
-    } else if (USER_RNAME.equals(name) && (!(USER_PASSWORD.equals("pass") ))){
+    } else if (USER_RNAME.equals(name) && (!(USER_PASSWORD.equals(pass)) )){
     System.out.println(CONST_MSG_ERROR_PASS); 
 
         // ④ 「USER_PASSWORD」の値のみ等しい場合。
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
-    } else if (!(USER_RNAME.equals(name)) && USER_PASSWORD.equals("pass")) {
+    } else if (!(USER_RNAME.equals(name)) && USER_PASSWORD.equals(pass)) {
     System.out.println(CONST_MSG_ERROR_NAME); 
 
 
